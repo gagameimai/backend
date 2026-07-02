f86752276bca673388bce60004229aa08474f66b
Meimai <mm08168168@gmail.com>
2026-07-02 18:52:02 +0800

    Add API file in route folder

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('question', 'QuestionController@get');
+Route::get('resource', 'ResourceController@get');
+Route::get('partner', 'PartnerController@get');
+Route::get('fitting', 'FittingController@get');
+Route::get('fitting/{id}', 'FittingController@detail');
+Route::get('blindspot', 'BlindSpotController@get');
+Route::get('blindspot/{id}', 'BlindSpotController@detail');
+Route::get('carframe', 'CarFrameController@get');
+Route::get('carframe/{id}', 'CarFrameController@detail');
+Route::get('multimedia', 'MultiMediaController@get');
+Route::get('multimedia/{id}', 'MultiMediaController@detail');
+Route::get('banner', 'BannerController@get');
+Route::get('website', 'WebsiteController@get');
+Route::get('search', 'SearchController@get');
+Route::get('car', 'CarController@get');
